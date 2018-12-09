c3b1d394459f33c14c23c67810486d890cc10cd6
Ekaterina Ryzhova <rubicante@icloud.com>
2018-12-09 05:23:10 +0300

    add and configure LoginVC

---
+++ EventCalendar/LoginVC.swift
@@ -0,0 +1,35 @@
+//
+//  LoginVC.swift
+//  EventCalendar
+//
+//  Created by kathelyss on 09/12/2018.
+//  Copyright © 2018 Екатерина Рыжова. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.isUserInteractionEnabled = true
+        let tapRecognizer = UITapGestureRecognizer()
+        tapRecognizer.addTarget(self, action: #selector(closeKeyboard))
+        view.addGestureRecognizer(tapRecognizer)
+        loginButton.layer.cornerRadius = 10
+        loginButton.layer.borderWidth = 1
+        loginButton.layer.borderColor = #colorLiteral(red: 0.07649140192, green: 0.6212597551, blue: 0.6272005793, alpha: 1).cgColor
+    }
+    
+    @objc
+    func closeKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func tapLoginButton(_ sender: UIButton) {
+    }
+}
