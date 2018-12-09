7201548537531f8a93fce73de81ecf14bc6d5a60
Ekaterina Ryzhova <rubicante@icloud.com>
2018-12-09 22:39:35 +0300

    setup calendar for current month

--- EventCalendar/CalendarVC.swift
+++ EventCalendar/CalendarVC.swift
@@ -13,8 +13,19 @@ class CalendarVC: UIViewController {
     @IBOutlet var monthNameLabel: UILabel!
     @IBOutlet var collectionView: UICollectionView!
     
+    let monthArray = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+                      "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+    let numberOfDaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    var currentMonthIndex: Int = 0
+    var currentYearIndex: Int = 0
+    var todaysDate = 0
+    var firstWeekDayOfMonth = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.allowsMultipleSelection = false
+        
+        setupCalendar()
         let addEventButton = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50)) // hidden when observe friend's calendar
         addEventButton.setTitle("⊕", for: .normal)
         addEventButton.setTitleColor(#colorLiteral(red: 0.07649140192, green: 0.6212597551, blue: 0.6272005793, alpha: 1), for: .normal)
@@ -22,6 +33,7 @@ class CalendarVC: UIViewController {
         addEventButton.contentHorizontalAlignment = .left
         addEventButton.addTarget(self, action: #selector(addEvent), for: .touchUpInside)
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: addEventButton)
+        
         let futureEventsButton = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
         futureEventsButton.setTitle("≡", for: .normal)
         futureEventsButton.setTitleColor(#colorLiteral(red: 0.07649140192, green: 0.6212597551, blue: 0.6272005793, alpha: 1), for: .normal)
@@ -31,6 +43,28 @@ class CalendarVC: UIViewController {
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(customView: futureEventsButton)
     }
 
+    private func setupCalendar() {
+        currentMonthIndex = Calendar.current.component(.month, from: Date()) - 1
+        currentYearIndex = Calendar.current.component(.year, from: Date())
+        todaysDate = Calendar.current.component(.day, from: Date())
+        
+        let date = Calendar.current.component(.month, from: Date()) - currentMonthIndex
+        firstWeekDayOfMonth = getFirstWeekDay() // get date
+        monthNameLabel.text = "\(monthArray[currentMonthIndex]) \(currentYearIndex)" //
+    }
+    
+    func didChangeMonthIndex(monthIndex: Int, year: Int) {
+        currentMonthIndex = monthIndex
+        currentYearIndex = year
+        firstWeekDayOfMonth = getFirstWeekDay() //
+        collectionView.reloadData()
+        
+    }
+    
+    func getFirstWeekDay() -> Int {
+        return Date().startOfMonth() - 1 // sunday is 7th day
+    }
+    
     @objc
     func addEvent() {
         performSegue(withIdentifier: "ToEventVC", sender: self)
@@ -52,9 +86,23 @@ class CalendarVC: UIViewController {
     }
     
     @IBAction func tapPreviousMonthButton(_ sender: UIButton) {
+        currentMonthIndex -= 1
+        if currentMonthIndex < 0 {
+            currentMonthIndex = 11
+            currentYearIndex -= 1
+        }
+        monthNameLabel.text = "\(monthArray[currentMonthIndex]) \(currentYearIndex)"
+        didChangeMonthIndex(monthIndex: currentMonthIndex, year: currentYearIndex)
     }
     
     @IBAction func tapNextMonthButton(_ sender: UIButton) {
+        currentMonthIndex += 1
+        if currentMonthIndex > 10 {
+            currentMonthIndex = 0
+            currentYearIndex += 1
+        }
+        monthNameLabel.text = "\(monthArray[currentMonthIndex]) \(currentYearIndex)"
+        didChangeMonthIndex(monthIndex: currentMonthIndex, year: currentYearIndex)
     }
 
 }
@@ -75,13 +123,25 @@ extension CalendarVC: UICollectionViewDelegate {
 extension CalendarVC: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView,
                         numberOfItemsInSection section: Int) -> Int {
-        return 35
+        return 7 * 6 // 7 days * 6 weeks (max in 1 month)
     }
     
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return collectionView.dequeueReusableCell(withReuseIdentifier: "MonthDayCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MonthDayCell",
+                                                      for: indexPath) as! MonthDayCell
+        cell.layer.cornerRadius = 5
+        if indexPath.item < firstWeekDayOfMonth - 1 ||
+            indexPath.item > numberOfDaysInMonth[currentMonthIndex] + firstWeekDayOfMonth - 2 {
+            cell.isHidden = true
+        } else {
+            let calcDate = indexPath.row - firstWeekDayOfMonth + 2
+            cell.isHidden = false
+            cell.dayNumberLabel.text = "\(calcDate)"
+        }
+        return cell
     }
+    
     
     func collectionView(_ collectionView: UICollectionView,
                         viewForSupplementaryElementOfKind kind: String,
@@ -92,6 +152,29 @@ extension CalendarVC: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = #colorLiteral(red: 0.07649140192, green: 0.6212597551, blue: 0.6272005793, alpha: 0.5)
         performSegue(withIdentifier: "ToFutureEventsVC", sender: self)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.backgroundColor = #colorLiteral(red: 0.1101291651, green: 0.8944641674, blue: 0.9030175209, alpha: 0.1)
+    }
+}
+
+// allows to get first day of month
+extension Date {
+    func startOfMonth() -> Int {
+        guard let interval = Calendar.current.dateInterval(of: .month, for: Date()) else {
+            print("Error!")
+            return 0
+        }
+        
+        return interval.start.weekDay
+    }
+    
+    var weekDay: Int {
+        return Calendar.current.component(.weekday, from: self)
     }
 }
